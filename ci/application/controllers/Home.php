ea2f187552298eb9995b161523d2c36dc1c2e6fd
Ana <anajuliabravoleite@gmail.com>
2017-12-29 18:19:15 +0000

    melhoria na pag de usuario cadastrado com sucesso

--- ci/application/controllers/Home.php
+++ ci/application/controllers/Home.php
@@ -4,23 +4,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
 	
-	public function index()
-	{
+	public function index() {
 		$this->load->view('header');
 		$this->load->view('home');
 		$this->load->view('footer');
 	}
 	
-	
-
-	
-	
-	
-	public function form(){
+	public function form() {
 		$data = array();
 		$data["msg"] = $this->session->userdata("msg");
 		$this->load->library('session');
-		$this->load->view("cadastrado",$data);
+		$this->load->view('header');
+		$this->load->view('cadastrado',$data);
+		$this->load->view('footer');
 		$this->session->unset_userdata("msg");
 	
 	}
